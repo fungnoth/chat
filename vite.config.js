7f25c3124591d3baec9f6d811b534d24ed5feb46
Fung <fungnoth@gmail.com>
2025-07-13 16:02:48 +0100

    set github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/chat/', // Replace 'chat' with your actual repository name
   server: {
     port: 3000,
     open: true
